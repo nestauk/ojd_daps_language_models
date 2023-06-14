53f9bd15f5747d267805c8cd66650b4423d01909
India Kerle <india.kerle@nesta.org.uk>
2023-06-14 16:28:45 +0100

    change memory resource after chat with solomon

--- ojd_daps_language_models/pipeline/train_model/ojobert/ojobert_flow.py
+++ ojd_daps_language_models/pipeline/train_model/ojobert/ojobert_flow.py
@@ -146,7 +146,7 @@ class OjoBertFlow(FlowSpec):
 
         self.next(self.train_bert_model)
 
-    @batch(gpu=1, memory=60000, cpu=10, queue="job-queue-GPU-nesta-metaflow")
+    @batch(gpu=1, memory=80000, cpu=8, queue="job-queue-GPU-nesta-metaflow")
     @step
     def train_bert_model(self):
         """Train a BERT-like model"""
