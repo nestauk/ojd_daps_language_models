8c502adafe378bdd5b83998ab0bb0cf4855434bf
India Kerle <india.kerle@nesta.org.uk>
2024-04-30 14:50:14 +0100

    refactor skillner; initial commit

---
+++ skillner/multiskills/README.md
@@ -0,0 +1,40 @@
+# 🤔 Multi-Skill Classification
+
+Scripts in this directory train a Support Vector Machine (SVM) classifier to predict whether an extracted 'SKILL' span contains multiple skills or not. 
+
+The features used for training the classifier include:
+1. The number of tokens in the 'SKILL' span;
+2. binary encoding if the token "and" is present in the 'SKILL' span and;
+3. binary encoding if the token "," is present in the 'SKILL' span.
+
+To run the flow in production locally, run:
+
+`python multiskill_flow.py --package-suffixes=.txt run --production=True`
+
+You will need access to Nesta's s3 bucket to train the model. 
+
+If you're happy with the evaluation metrics, you can push the model to HuggingFace by running:
+
+`python multiskill_flow.py --package-suffixes=.txt run --production=True --push_to_hf=True`
+
+You will need access to Nesta's s3 bucket to train the model and be logged into huggingface hub to push the model. 
+
+## 📠 Using the model
+
+To use the model, you can load it from huggingface hub:
+
+```
+import joblib
+from pathlib import Path
+from skops.hub_utils import download
+
+ms_path = Path.cwd() / "outputs"
+download(repo_id="nestauk/multiskill-classifier", dst=ms_path)
+model = joblib.load(
+	ms_path / "skops-pl2mwv65.pkl"
+)
+# only load pickle files from sources you trust
+# read more about it here https://skops.readthedocs.io/en/stable/persistence.html
+```
+
+The model metrics are also reported from huggingface hub. 
---
+++ skillner/multiskills/multiskill_utils.py
@@ -0,0 +1,320 @@
+import dataclasses
+import difflib
+import re
+from typing import Dict, List, Tuple, Union
+
+from toolz import pipe
+
+
+## define config
+@dataclasses.dataclass
+class TrainConfig:
+    random_seed: int = 42
+    test_size: float = 0.25
+    kernel: str = "linear"
+    class_weight: str = "balanced"
+
+
+@dataclasses.dataclass
+class DataConfig:
+    bucket_name: str = "open-jobs-lake"
+    data_path: str = (
+        "escoe_extension/outputs/labelled_job_adverts/combined_labels_20220824.json"
+    )
+    all_labels: List[str] = dataclasses.field(
+        default_factory=lambda: ["SKILL", "MULTISKILL", "EXPERIENCE", "BENEFIT"]
+    )
+
+
+# define our config
+@dataclasses.dataclass
+class Config:
+    model_name: str = "nestauk/multiskill-classifier"
+    train: TrainConfig = TrainConfig()
+    data: DataConfig = DataConfig()
+
+
+config: Config = Config()
+
+compiled_missing_space_pattern = re.compile("([a-z])([A-Z])")
+compiled_nonalphabet_nonnumeric_pattern = re.compile(r"([^a-zA-Z0-9] )")
+exception_camelcases = [
+    "JavaScript",
+    "WordPress",
+    "PowerPoint",
+    "CloudFormation",
+    "CommVault",
+    "InDesign",
+    "GitHub",
+    "GitLab",
+    "DevOps",
+    "QuickBooks",
+    "TypeScript",
+    "XenDesktop",
+    "DevSecOps",
+    "CircleCi",
+    "LeDeR",
+    "CeMap",
+    "MavenAutomation",
+    "SaaS",
+    "iOS",
+    "MySQL",
+    "MongoDB",
+    "NoSQL",
+    "GraphQL",
+    "VoIP",
+    "PhD",
+    "HyperV",
+    "PaaS",
+    "ArgoCD",
+    "WinCC",
+    "AutoCAD",
+]
+trim_chars = [" ", ".", ",", ";", ":", "\xa0"]
+
+
+### FUNCTIONS ###
+
+
+def edit_ents(text, orig_ents):
+    """
+    A function to fix the text and entity spans,
+    will remove trailing whitespace/punctuation
+    from the text and spans
+    """
+
+    editted = False
+    # Don't include trailing whitespace from entity spans
+    trimmed_ents = []
+    for b, e, l in orig_ents:
+        if text[b] in trim_chars:
+            new_b = b + 1
+            editted = True
+        else:
+            new_b = b
+
+        if text[e - 1] in trim_chars:
+            new_e = e - 1
+            editted = True
+        else:
+            new_e = e
+        trimmed_ents.append((new_b, new_e, l))
+    return trimmed_ents, editted
+
+
+def fix_entity_annotations(text, ents):
+    """
+    Clean the text and entity spans for cases
+    where the entity ends but the next character is not a space
+    e.g. "this is OK you need to fixMe please and hereToo please"
+    ents = [(8, 10, "LABEL"), (15, 26, "LABEL"), (36,44,"LABEL")]
+
+    Also:
+    - if start or end of entity is a space then trim it
+    """
+    ent_additions = [0] * len(ents)
+    insert_index_space = []
+    for i, (b, e, l) in enumerate(ents):
+        # If the char before the start of this span is not a space,
+        # Then update from this ent onwards
+        if b != 0:
+            if text[b - 1] != " ":
+                ent_additions[i:] = [ea + 1 for ea in ent_additions[i:]]
+                insert_index_space.append(b)
+
+        # If the next char after this span is not a space,
+        # then update the start and endings of all entities after this
+        if (e) < len(text):
+            if text[e] != " ":
+                ent_additions[(i + 1) :] = [ea + 1 for ea in ent_additions[(i + 1) :]]
+                insert_index_space.append(e)
+
+    # Fix entity spans
+    new_ents = []
+    for (b, e, l), add_n in zip(ents, ent_additions):
+        new_ents.append((b + add_n, e + add_n, l))
+
+    # Add spaces in the correct places
+    b = 0
+    new_texts = []
+    for e in insert_index_space:
+        new_texts.append(text[b:e])
+        b = e
+    new_texts.append(text[b:])
+    new_text = " ".join(new_texts)
+
+    editted = True
+    trimmed_ents = new_ents
+    while editted:
+        trimmed_ents, editted = edit_ents(new_text, trimmed_ents)
+
+    return new_text, trimmed_ents
+
+
+def pad_punctuation(text):
+    """Pad punctuation marks with spaces (to facilitate lemmatisation)"""
+    text = compiled_nonalphabet_nonnumeric_pattern.sub(r" \1 ", text)
+    return text
+
+
+def detect_camelcase(text):
+    """
+    Splits a word written in camel-case into separate sentences. This fixes a case
+    when the last word of a sentence in not seperated from the capitalised word of
+    the next sentence. This tends to occur with enumerations.
+
+    For example, the string "skillsBe" will be converted to "skills. Be"
+
+    Some camelcases are allowed though - these are found and replaced. e.g. JavaScript
+
+    Note that the present solution doesn't catch all such cases (e.g. "UKSkills")
+
+    Reference: https://stackoverflow.com/questions/1097901/regular-expression-split-string-by-capital-letter-but-ignore-tla
+    """
+    text = compiled_missing_space_pattern.sub(r"\1. \2", str(text))
+    for exception in exception_camelcases:
+        exception_cleaned = compiled_missing_space_pattern.sub(r"\1. \2", exception)
+        if exception_cleaned in text:
+            text = text.replace(exception_cleaned, exception)
+
+    return text
+
+
+def clean_text_pipeline(text):
+    """
+    Pipeline for preprocessing online job vacancy and skills-related text.
+    This should ONLY insert characters (eg spaces, fullstops) - not delete or replace any.
+    This is because when it comes to cross referencing the cleaned text with entity spans
+    our algorithm depends on only insertion.
+
+    Args:
+            text (str): Text to be processed via the pipeline
+    """
+    return pipe(
+        text,
+        detect_camelcase,
+        pad_punctuation,  # messes up entity spans
+    )
+
+
+def get_old2new_chars_dict(orig_text, new_text):
+    """
+    This is a function to map the orig_text character indices to the new_text indices
+    e.g.
+    orig_text = "abcd"
+    new_text = "ab cd"
+    old2new_chars_dict = {0:0, 1:1, 2:3, 3:4}
+    """
+    seq_matcher = difflib.SequenceMatcher(None, orig_text, new_text)
+    old2new_chars_dict = {}
+    for tag, i1, i2, j1, j2 in seq_matcher.get_opcodes():
+        if tag == "equal":
+            step_up = j1 - i1
+            for i in range(i1, i2):
+                old2new_chars_dict[i] = i + step_up
+        elif tag == "insert":
+            old2new_chars_dict[i1] = j1
+        elif tag == "replace":
+            # This shouldnt really be happening since our cleaning is only
+            # inserting, but sometimes it does categorise as "replace" in cases where it
+            # thinks adding whitespace to either side is a replacement
+            # e.g. "abcd" -> " abcd "
+            step_up = j1 - i1
+            for i in range(i1, i2):
+                old2new_chars_dict[i] = i + 1 + step_up
+
+    return old2new_chars_dict
+
+
+def fix_all_formatting(text, ents):
+    new_text = clean_text_pipeline(text)
+    old2new_chars_dict = get_old2new_chars_dict(text, new_text)
+
+    new_ents = []
+    num_index_problems = 0
+    for b, e, t in ents:
+        new_b = old2new_chars_dict.get(b)
+        new_e = old2new_chars_dict.get(e)
+        if new_b and new_e:
+            new_ents.append((new_b, new_e, t))
+        else:
+            num_index_problems += 1
+
+    if num_index_problems != 0:
+        print(
+            f"Problems with {num_index_problems} entity spans - these will be left out of any training or testing"
+        )
+
+    return new_text, new_ents
+
+
+def clean_entities_text(text, ents):
+    text, ents = fix_all_formatting(text, ents)
+    text, ents = fix_entity_annotations(
+        text, ents
+    )  # apply after to deal with the padding
+    return text, ents
+
+
+def _process_data(
+    job_advert_labels: Dict[str, str], all_labels: List[str]
+) -> Tuple[str, List[Tuple[int, int, str]], List[str]]:
+    """
+    Process the raw labelled data but ensure that the span indices are
+        correct given they have been labelled using Prodigy and
+        label studio.
+
+    Args:
+        job_advert_labels : dict
+            The raw label-studio labelled data for one job advert
+        all_labels : list
+            The list of all labels given to entities
+
+    Returns:
+        text : str
+            The cleaned job advert text
+        ent_list : list
+            The entity span list (modified after cleaning the text)
+            this is in the form [(start_char, end_char, label),...]
+        all_labels : list
+            The list of all labels given to entities
+    """
+    text = job_advert_labels["text"]
+    ent_tags = job_advert_labels["labels"]
+
+    ent_list = []
+    for ent_tag in ent_tags:
+        ent_tag_value = ent_tag["value"]
+        label = ent_tag_value["labels"][0]
+        ent_list.append((ent_tag_value["start"], ent_tag_value["end"], label))
+        if label not in all_labels:
+            all_labels.add(label)
+
+    # The entity list is in the order labelled not in
+    # character order
+    ent_list.sort(key=lambda y: y[0])
+
+    if job_advert_labels.get("type") == "label-studio":
+        # Label-studio- specific cleaning, won't work (and not needed) for Prodigy
+        text, ent_list = clean_entities_text(text, ent_list)
+
+    return text, ent_list
+
+
+def _transform_data(entity_list: Union[List[str], str]) -> List[int]:
+    """Transform text data into a list of numerical features.
+
+    Args:
+        entity_list (Union[List[str], str]): Entity list.
+
+    Returns:
+        List[int]: List of numerical features.
+    """
+
+    entity_list = [entity_list] if isinstance(entity_list, str) else entity_list
+
+    entity_vec = []
+    for entity in entity_list:
+        entity_vec.append([len(entity), int(" and " in entity), int("," in entity)])
+
+    return entity_vec
